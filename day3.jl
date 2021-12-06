dd80612f48dd0e60f53239e9349358708985a7b2
richardt94 <29562583+richardt94@users.noreply.github.com>
2021-12-06 15:32:29 +1100

    Finish day 3

--- day3.jl
+++ day3.jl
@@ -23,3 +23,101 @@ poweruse(tdata)
 data = readlines("input3.txt")
 poweruse(data)
 ##
+function bits2num(bs)
+    b = 0
+    num = 0
+    for bit = reverse(bs)
+        num += 2^b * (bit=='1')
+        b += 1
+    end
+    num
+end
+##
+function lifesupport(data)
+    n_num = length(data)
+    eliminated = zeros(Bool, n_num)
+    nbits = length(data[1])
+    n_elim = 0
+    for b = 1:nbits
+        mb = false
+        count = 0
+        for (i, n) = enumerate(data)
+            if eliminated[i]
+                continue
+            end
+            if n[b] == '1'
+                count += 1
+            end
+        end
+        if count >= (n_num - n_elim)/2
+            mb = true
+        end
+        for (i,n) = enumerate(data)
+            if eliminated[i]
+                continue
+            end
+            if (n[b] == '1') != mb
+                eliminated[i] = true
+                n_elim += 1
+            end
+        end
+        if n_elim >= n_num - 1
+            break
+        end
+    end
+    
+    #retrieve o2
+    o2 = ""
+    for (i, n) = enumerate(data)
+        if ~eliminated[i]
+            o2 = n
+        end
+    end
+
+    eliminated = zeros(Bool, n_num)
+    n_elim = 0
+    for b = 1:nbits
+        mb = false
+        count = 0
+        for (i, n) = enumerate(data)
+            if eliminated[i]
+                continue
+            end
+            if n[b] == '1'
+                count += 1
+            end
+        end
+        if count < (n_num - n_elim)/2
+            mb = true
+        end
+        for (i,n) = enumerate(data)
+            if eliminated[i]
+                continue
+            end
+            if (n[b] == '1') != mb
+                eliminated[i] = true
+                n_elim += 1
+            end
+        end
+        if n_elim >= n_num - 1
+            break
+        end
+    end
+
+    co2 = ""
+    for (i, n) = enumerate(data)
+        if ~eliminated[i]
+            co2 = n
+        end
+    end
+
+    bits2num(o2)*bits2num(co2)
+
+end
+
+##
+
+lifesupport(tdata)
+
+##
+lifesupport(data)
