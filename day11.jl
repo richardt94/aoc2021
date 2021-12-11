307b3cac5ba0e347afe0ca9c3b331d7c75478022
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-12-11 19:31:32 +1100

    Better stack solution to day 11

--- day11.jl
+++ day11.jl
@@ -10,36 +10,31 @@ function simulate(data, nsteps)
     end
 
     nflash = 0
-    for _ in 1:nsteps
+    for _ = 1:nsteps
         board .+= 1
-        keep_flashing = true
-        already_flashed = Set()
-        while keep_flashing
-            keep_flashing = false
-            for y in 1:size(board,1)
-                for x in 1:size(board,2)
-                    if ~in((y,x), already_flashed) && board[y,x] > 9
-                        board[y, x] = 0
-                        neighbours = [(y-1, x-1), (y-1, x), (y-1, x+1),
-                                      (y, x-1),             (y, x+1),
-                                      (y+1, x-1), (y+1,x),  (y+1, x+1)]
-                        for neigh in neighbours
-                            if (neigh[1] <= size(board,1) && neigh[1] >= 1
-                                && neigh[2] <= size(board,2) && neigh[2] >= 1
-                                && ~in(neigh, already_flashed))
-                                board[neigh...] += 1
-                            end
-                        end
-                        push!(already_flashed, (y,x))
-                        keep_flashing = true
-                        nflash += 1
-                    end
+        stack = []
+        [board[y,x] == 10 ? push!(stack, (y,x)) : nothing for y = 1:size(board,1), x = 1:size(board,2)]
+        while length(stack) > 0
+            (y,x) = pop!(stack)
+            board[y,x] = 0
+            nflash += 1
+            neighbours = [(y-1, x-1), (y-1, x), (y-1, x+1),
+                          (y, x-1),             (y, x+1),
+                          (y+1, x-1), (y+1,x),  (y+1, x+1)]
+            for (y2, x2) in neighbours
+                (y2 > size(board,1) || y2 < 1 || x2 > size(board,2) || x2 < 1) && continue
+                (board[y2,x2] == 0 || board[y2,x2] == 10) && continue
+                board[y2,x2] += 1
+                if board[y2,x2] == 10
+                    push!(stack, (y2,x2))
                 end
             end
         end
     end
     nflash
 end
+##
+simulate(tdata, 100)
 ##
 simulate(data, 100)
 ##
@@ -54,34 +49,29 @@ function synchronise(data)
     s = 1
     while true
         board .+= 1
-        keep_flashing = true
-        already_flashed = Set()
-        while keep_flashing
-            keep_flashing = false
-            for y in 1:size(board,1)
-                for x in 1:size(board,2)
-                    if ~in((y,x), already_flashed) && board[y,x] > 9
-                        board[y, x] = 0
-                        neighbours = [(y-1, x-1), (y-1, x), (y-1, x+1),
-                                      (y, x-1),             (y, x+1),
-                                      (y+1, x-1), (y+1,x),  (y+1, x+1)]
-                        for neigh in neighbours
-                            if (neigh[1] <= size(board,1) && neigh[1] >= 1
-                                && neigh[2] <= size(board,2) && neigh[2] >= 1
-                                && ~in(neigh, already_flashed))
-                                board[neigh...] += 1
-                            end
-                        end
-                        push!(already_flashed, (y,x))
-                        keep_flashing = true
-                        length(already_flashed) == length(board) && return s
-                    end
+        stack = []
+        nflash = 0
+        [board[y,x] == 10 ? push!(stack, (y,x)) : nothing for y = 1:size(board,1), x = 1:size(board,2)]
+        while length(stack) > 0
+            (y,x) = pop!(stack)
+            board[y,x] = 0
+            nflash += 1
+            neighbours = [(y-1, x-1), (y-1, x), (y-1, x+1),
+                          (y, x-1),             (y, x+1),
+                          (y+1, x-1), (y+1,x),  (y+1, x+1)]
+            for (y2, x2) in neighbours
+                (y2 > size(board,1) || y2 < 1 || x2 > size(board,2) || x2 < 1) && continue
+                (board[y2,x2] == 0 || board[y2,x2] == 10) && continue
+                board[y2,x2] += 1
+                if board[y2,x2] == 10
+                    push!(stack, (y2,x2))
                 end
             end
         end
+        nflash == length(board) && break
         s += 1
     end
-    
+    s
 end
 ##
 synchronise(tdata)
