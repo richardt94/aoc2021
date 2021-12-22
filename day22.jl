2a85f178220dd6147f7f220c6748bf017e5949bf
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-12-22 21:51:56 +1100

    Make day 22 part 1 faster

--- day22.jl
+++ day22.jl
@@ -14,24 +14,7 @@ end
 ##
 tr = parse_rules(test_input)
 ##
-function num_on(rules)
-    n = 0
-    for x=-50:50,y=-50:50,z=-50:50
-        for rule in reverse(rules)
-            if x >= rule[2] && x <= rule[3] && y >= rule[4] && y <= rule[5] && z >= rule[6] && z <= rule[7]
-                rule[1] && (n += 1)
-                break
-            end
-        end
-    end
-    n
-end
-##
-num_on(parse_rules(test_input))
-##
 rr = parse_rules(readlines("input22.txt"))
-##
-num_on(rr)
 ##
 struct Cuboid
     x1::Integer
@@ -94,9 +77,9 @@ difference(c1,c2)
 ##
 size(c::Cuboid) = (c.x2 - c.x1 + 1) * (c.y2 - c.y1 + 1) * (c.z2 - c.z1 + 1)
 ##
-function num_on_all(rules)
+function calc_on_cubes(rules)
     on = []
-    for (i,rule) in enumerate(rules)
+    for rule in rules
         c2 = Cuboid(rule[2:end]...)
         new_on = []
         for c1 in on
@@ -105,10 +88,43 @@ function num_on_all(rules)
         rule[1] && push!(new_on, c2)
         on = new_on
     end
-    sum(size.(on))
+    on
 end
+num_on_all(rules) = sum(size.(calc_on_cubes(rules)))
 ##
 num_on_all(tr)
 ##
 rr = parse_rules(readlines("input22.txt"))
+##
 num_on_all(rr)
+##
+function intersection(c1::Cuboid, c2::Cuboid)
+    x1 = max(c1.x1, c2.x1)
+    x2 = min(c1.x2, c2.x2)
+    y1 = max(c1.y1, c2.y1)
+    y2 = min(c1.y2, c2.y2)
+    z1 = max(c1.z1, c2.z1)
+    z2 = min(c1.z2, c2.z2)
+    (x2 < x1 || y2 < y1 || z2 < z1) && return nothing
+    Cuboid(x1,x2,y1,y2,z1,z2)
+end
+
+region = Cuboid(-50,50,-50,50,-50,50)
+function calc_on_cubes_small(rules)
+    on = []
+    for rule in rules
+        c2 = intersection(region, Cuboid(rule[2:end]...))
+        isnothing(c2) && continue
+        new_on = []
+        for c1 in on
+            push!(new_on, difference(c1,c2)...)
+        end
+        rule[1] && push!(new_on, c2)
+        on = new_on
+    end
+    on
+end
+
+num_on_small(rules) = sum(size.(calc_on_cubes_small(rules)))
+##
+num_on_small(rr)
